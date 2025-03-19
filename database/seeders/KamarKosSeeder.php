0f26cf28ff9bb1c20bba51158db18b34297b5956
Defri Indra Mahardika <defrindr@gmail.com>
2025-03-19 12:17:12 +0700

    Add KamarKos seeder and update admin layout title format

--- database/seeders/KamarKosSeeder.php
+++ database/seeders/KamarKosSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\KamarKos;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,17 @@ class KamarKosSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        KamarKos::create([
+            'name' => 'Kamar 1',
+            'description' => 'Kamar 1',
+            'price' => 1000000,
+            'available' => true
+        ]);
+        KamarKos::create([
+            'name' => 'Kamar 2',
+            'description' => 'Kamar 2',
+            'price' => 1500000,
+            'available' => true
+        ]);
     }
 }
